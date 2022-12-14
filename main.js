2af21555aa8e89fdcfde2a2a00ac1fdbc71af7a9
Sikor08 <sikor08@gmail.com>
2022-12-14 12:44:50 +0300

    baseStructure

--- main.js
+++ main.js
@@ -1,75 +1,58 @@
-/**dom elems */
-let input = document.querySelector('.notePad__input');
-let addBtn = document.querySelector('.notePad__btn');
-let toDoList = document.querySelector('.toDoList');
+// App Data Model
 
+/*
+  type Todo = {
+    id: number
+    name: string
+    completed: boolean
+    createdAt: Date
+  }
 
-/**variables */
-let genId = 100;
-// let itemCounter = 0;
-
-// Всегда свежая дата
-let date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-let noteHeading = document.querySelector('.notePad__heading');
-noteHeading.insertAdjacentHTML('beforeend', date)
-
-// -------------------------------------------------------------
-
-
-
-/** 
- * diabling button when input-field empty
+  Array<Todo>
 */
-input.addEventListener('input', (e) => {
-    input.value !== ''? addBtn.removeAttribute('disabled'): addBtn.setAttribute('disabled', '')
-}) 
-/**
- * 
- * @returns lauout
- */
-let render = () =>{
-    return `<li class="toDoListItem" data-id=${genId}> 
-    <div class="toDoListItem__check">
-        <input class="toDoListItem__checkbox" type="checkbox">
-        <span class="toDoListItem__itemCounter">-</span>
-    </div>
-    <p class="toDoListItem__title">${input.value}</p>
-    <button type="button" class="btn-close toDoListItem__delBtn" aria-label="Close" data-id="${genId}"></button>
-</li>
-`
+
+// App State
+
+const todos = [
+    {
+        id: 1,
+        name: 'Learn JavaScript',
+        completed: false,
+        createdAt: new Date(),
+    },
+    {
+        id: 2,
+        name: 'Become a FrontEnd master',
+        completed: false,
+        createdAt: new Date(2022, 5, 1)
+    },
+    {
+        id: 3,
+        name: 'Clean my room',
+        completed: true,
+        createdAt: new Date(2022, 11, 1),
+    }
+]
+
+const todoListEl = document.querySelector('#todoList')
+
+const render = () => {
+    for (const todo of todos) {
+        const liEl = document.createElement('li')
+
+        const checkboxEl = document.createElement('input')
+        checkboxEl.type = 'checkbox'
+        const labelEl = document.createElement('label')
+        labelEl.textContent = todo.name
+        labelEl.prepend(checkboxEl)
+        liEl.append(labelEl)
+
+        const dateEl = document.createElement('span')
+        dateEl.textContent = todo.createdAt.toLocaleString()
+        liEl.append(dateEl)
+
+        todoListEl.append(liEl)
+    }
 }
 
-/** 
- * click handler
- */
-addBtn.addEventListener('click', () => {
-    genId +=10 //Увеличиваем счётчик id
-
-    toDoList.classList.remove('hidden'); // 
-    toDoList.insertAdjacentHTML('beforeend', render()); // вставляем вёрстку
-    let actItems = document.querySelectorAll('.toDoListItem');
-    actItems.forEach(item => {
-        item.classList.add('toDoListItemAppear')
-    })
-
-    input.value = ''; // очищаем инпут
-    addBtn.setAttribute('disabled', '') // блокируем кнопку
-    // itemCounter +=1
-    let AllBtnsClose = document.querySelectorAll('.btn-close');
-    AllBtnsClose.forEach(btn => {
-        btn.addEventListener('click', () => {
-            // itemCounter -=1
-            actItems.forEach(item => {
-                if(item.getAttribute('data-id') == btn.getAttribute('data-id')){
-                    item.classList.add('swirl-out-bck')
-                    setTimeout(() => {
-                        item.remove()
-                    },'1000');
-                    
-                }
-            })
-        })
-    })
-    })
-    
-
+render()
---
+++ todo.md
@@ -0,0 +1,11 @@
+## Features
+
+- heading should show number of todo items (Todos: (7))
+- add button should have the next todo item number [# add todo 8] 
+- create a todo with a name
+- update todo name
+- search for a todo with a name
+- delete a todo
+- mark todo as finished/unfinished
+- filter by (finished/unfinished) status
+- filter by date created
