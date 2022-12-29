9b0ffb41098b94e52646b55f45e099d291a8eaf9
Sikor08 <sikor08@gmail.com>
2022-12-29 22:22:35 +0300

    delete todo complete

--- main.js
+++ main.js
@@ -13,46 +13,112 @@
 
 // App State
 
-const todos = [
-    {
-        id: 1,
-        name: 'Learn JavaScript',
-        completed: false,
-        createdAt: new Date(),
-    },
-    {
-        id: 2,
-        name: 'Become a FrontEnd master',
-        completed: false,
-        createdAt: new Date(2022, 5, 1)
-    },
-    {
-        id: 3,
-        name: 'Clean my room',
-        completed: true,
-        createdAt: new Date(2022, 11, 1),
+const createFormEl = document.querySelector('#createForm');
+const todoListEl = document.querySelector('#todoList');
+
+const createTodo = (name, completed) => {
+    return {
+        name,
+        completed,
+        id: crypto.randomUUID(),
+        createdAt: new Date()
     }
+
+}
+
+const todos = [
+    createTodo('Go shoping', false),
+    createTodo('Dog walking', false),
+    createTodo('Clean room', true),
 ]
 
-const todoListEl = document.querySelector('#todoList')
-
 const render = () => {
+    todoListEl.innerHTML = ''
     for (const todo of todos) {
-        const liEl = document.createElement('li')
+        const liEl = document.createElement('li');
+        if (todo.completed) {
+            liEl.classList.add('completed')
+        }
 
-        const checkboxEl = document.createElement('input')
-        checkboxEl.type = 'checkbox'
-        const labelEl = document.createElement('label')
+        const checkBoxEl = document.createElement('input');
+        checkBoxEl.type = 'checkbox';
+        checkBoxEl.checked = todo.completed;
+
+        checkBoxEl.addEventListener('input', () => {
+            todo.completed = !todo.completed
+            render()
+        })
+
+        const labelEl = document.createElement('label');
         labelEl.textContent = todo.name
-        labelEl.prepend(checkboxEl)
-        liEl.append(labelEl)
+        labelEl.prepend(checkBoxEl);
+        liEl.append(labelEl);
 
-        const dateEl = document.createElement('span')
+        const dateEl = document.createElement('span');
         dateEl.textContent = todo.createdAt.toLocaleString()
         liEl.append(dateEl)
+        todoListEl.append(liEl);
 
-        todoListEl.append(liEl)
+        const delBtn = document.createElement('button');
+        delBtn.classList.add('delBtn')
+        delBtn.textContent = 'Delete';
+
+        delBtn.addEventListener('click', () => {
+            const id = todo.id
+            const index = todos.findIndex(item => item.id === id)
+            todos.splice(index, 1)
+            render()
+        })
+        liEl.append(delBtn);
     }
 }
 
+createFormEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(createFormEl);
+    const name = formData.get('todoName');
+    todos.push(createTodo(name));
+    render();
+    createFormEl.reset()
+    console.log(todos)
+})
+
 render()
+
+// todos id
+
+// const nameToDelete = 'John'
+// const names = ['Alice', 'Bob', 'John'];
+
+// const index = names.indexOf(nameToDelete)
+// names.splice(index, 1)
+
+// console.log(names)
+
+// const nameToDelete = 'John'
+// const users = [{ name: 'Alice' }, { name: 'Bob' }, { name: 'John' }];
+
+// const index = users.findIndex(user => user.name === nameToDelete)
+// users.splice(index, 1)
+
+// for (let index = 0; index < users.length; index++) {
+//     const currentUser = users[index]
+//     if (currentUser.name === nameToDelete) {
+//         users.splice(index, 1)
+//     }
+// }
+
+// const index = users.indexOf(nameToDelete)
+// console.log({ index })
+// users.splice(index, 1)
+
+// console.log(users)
+// console.log(users.map(user => user.name))
+
+// linear search
+// for (let index = 0; index < names.length; index++) {
+//     const currentName = names[index]
+//     if (currentName === nameToDelete) {
+//         names.splice(index, 1)
+//     }
+// }
